ad8445b41e2cf332e058b561ac0551a55f83c467
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2025-10-09 00:21:37 +0300

    added delete and some other nicities

--- app/src/main/java/com/pierbezuhoff/define/MainActivity.kt
+++ app/src/main/java/com/pierbezuhoff/define/MainActivity.kt
@@ -1,30 +1,24 @@
 package com.pierbezuhoff.define
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.annotation.ColorInt
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
+import com.pierbezuhoff.define.ui.App
 import com.pierbezuhoff.define.ui.DefineViewModel
-import com.pierbezuhoff.define.ui.MainScreen
 import com.pierbezuhoff.define.ui.buildCustomTabsIntent
 import com.pierbezuhoff.define.ui.theme.DefineTheme
-import androidx.core.net.toUri
-import com.pierbezuhoff.define.ui.App
 import com.pierbezuhoff.define.ui.theme.LocalIsDarkTheme
 import kotlin.math.roundToInt
 
--- app/src/main/java/com/pierbezuhoff/define/ui/MainScreen.kt
+++ app/src/main/java/com/pierbezuhoff/define/ui/MainScreen.kt
@@ -6,12 +6,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -249,7 +247,7 @@ private fun QueryCard(
                     Icon(
                         painterResource(R.drawable.delete),
                         contentDescription = "Delete",
-                        tint = MaterialTheme.colorScheme.error.copy(alpha = 0.5f),
+                        tint = MaterialTheme.colorScheme.error.copy(alpha = 0.7f),
                     )
                 }
             }
@@ -260,7 +258,7 @@ private fun QueryCard(
 @Preview
 @Composable
 fun MainScreenPreview() {
-    DefineTheme {
+    DefineTheme(darkTheme = true) {
         MainScreen(
             queryVariants = listOf(
                 QueryVariant.GOOGLE_DEFINE,
