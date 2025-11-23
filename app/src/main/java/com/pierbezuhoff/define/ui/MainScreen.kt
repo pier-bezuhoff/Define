a09de26891c34538168e090cdad3e327f2537762
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2025-11-23 11:57:40 +0300

    proper resize on virtual kbd

--- app/src/main/java/com/pierbezuhoff/define/ui/MainScreen.kt
+++ app/src/main/java/com/pierbezuhoff/define/ui/MainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -130,7 +131,10 @@ private fun MainScreen(
     }
 
     Scaffold(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .imePadding()
+            .fillMaxSize()
+        ,
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
@@ -237,6 +241,7 @@ private fun MainScreen(
             Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
+
         ) {
             LazyColumn(
                 horizontalAlignment = Alignment.CenterHorizontally,
